d75a1078308903f9d1a5ee5ae43e1a742ebb7686
sumit kumar <rsumit123@gmail.com>
2021-05-10 07:15:11 +0530

    added hindu

--- download_f.py
+++ download_f.py
@@ -16,7 +16,7 @@ from scrapy.http import HtmlResponse
 def get_link(url,date):
     
     response = requests.get(url)
-    # with open('ff.html','w') as dp:
+    # with open('ff_ie.html','w') as dp:
     #     dp.write(response.text)
     
     response = HtmlResponse(url = url,body=response.text,encoding='utf-8')
@@ -61,28 +61,12 @@ def get_link(url,date):
 
     
     # return f_link
-def get_link_1(url,date):
-
-    headers = {
-    'authority': 'www.careerswave.in',
-    'cache-control': 'max-age=0',
-    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"',
-    'sec-ch-ua-mobile': '?0',
-    'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'sec-fetch-site': 'cross-site',
-    'sec-fetch-mode': 'navigate',
-    'sec-fetch-user': '?1',
-    'sec-fetch-dest': 'document',
-    'referer': 'https://www.google.com/',
-    'accept-language': 'en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7,fr;q=0.6',
-    'cookie': '__cfduid=ddad7bc73ee68465f10dda5074ac376681620399407; _ga=GA1.2.1463876823.1620399410; _gid=GA1.2.595933664.1620399410; __gads=ID=1c58b14dcb2ca70f-221950eedcc70072:T=1620399410:RT=1620399410:S=ALNI_MaMYuwfcNzd8kjYfGxMZHlBiXNc1Q',
-}
-
+######################################################################################
+def get_link_hindu(url,date):
+    
     response = requests.get(url)
-    with open('ff1.html','w') as dp:
-        dp.write(response.text)
+    # with open('ff_hindu.html','w') as dp:
+    #     dp.write(response.text)
     
     response = HtmlResponse(url = url,body=response.text,encoding='utf-8')
 
@@ -102,9 +86,9 @@ def get_link_1(url,date):
     link = None
     for i in range(16,1,-1):
         try:
-            text = response.xpath(f'//*[@id="post-810"]/div/p[{i}]/span/text()').extract()[0]
+            text = response.xpath(f'//*[@id="post-17698"]/div/p[{i}]/text()').extract()[0]
             if date.strip().lower() in text.strip().lower():
-                link = response.xpath(f'//*[@id="post-810"]/div/p[{i}]/span/a/@href').extract()[0]
+                link = response.xpath(f'//*[@id="post-17698"]/div/p[{i}]/a/@href').extract()[0]
                 print(link)
                 break
 
@@ -112,15 +96,19 @@ def get_link_1(url,date):
             print(e)
 
     if link is not None:
+        # f_link = link
 
         response = requests.get(link)
-        with open('ff.html','w') as dp:
-            dp.write(response.text)
+        # with open('ff.html','w') as dp:
+        #     dp.write(response.text)
         response = HtmlResponse(url = link,body=response.text,encoding='utf-8')
         f_link = response.xpath('//*[@id="iframe"]/@src').extract()[0]
         return f_link
     else:
         return 0
+
+
+########################################################################################
 
 
 
@@ -168,21 +156,25 @@ def send_mail(send_from, send_to, subject, message, files=[],
 def download_pdf():
     print("Checking paper availability..")
     url = "https://dailyepaper.in/indian-express-epaper/"
-    url1 = "https://www.careerswave.in/the-indian-express-newspaper-download/"
+    url_hindu = "https://dailyepaper.in/the-hindu-pdf-epaper-downloa-now/"
     date = datetime.datetime.today().strftime('%d %b %Y')
     tdate = datetime.datetime.today().strftime('%d-%m-%Y')
     ydate = (datetime.datetime.today() - datetime.timedelta(days=1)).strftime('%d-%m-%Y')
-    # date = "16 Apr 2021"
-    # tdate = "16-04-2021"
+    # date = "09 May 2021"
+    # tdate = "09-05-2021"
     # url = get_link(url,date)
     url = get_link(url,date)
-    if url !=0:
+    url_hindu = get_link_hindu(url_hindu,date)
+    print("IE == ",url)
+    print("TH == ",url_hindu)
+    if url !=0 or url_hindu !=0 :
         print("Paper found , downloading..")
 
-        print("downloading paper..."+"Newspaper_"+tdate+".pdf")
-        if not os.path.exists("Newspaper_"+tdate+".pdf"):
+        print("downloading paper..."+"IE_Newspaper_"+tdate+".pdf"+" , " +"TH_Newspaper_"+tdate+".pdf")
+        if not os.path.exists("IE_Newspaper_"+tdate+".pdf"):
             try:
-                os.remove('Newspaper_'+ydate+".pdf")
+                os.remove('IE_Newspaper_'+ydate+".pdf")
+                os.remove('TH_Newspaper_'+ydate+".pdf")
                 print("Newspaper deleted of date: "+ydate)
             except:
                 pass
@@ -190,17 +182,23 @@ def download_pdf():
     
         ###########################DOWNLOAD###################################################
         r = requests.get(url) 
-        with open("Newspaper_"+tdate+".pdf",'wb') as f: 
+        with open("IE_Newspaper_"+tdate+".pdf",'wb') as f: 
 
 
             f.write(r.content) 
-        print("sending email..."+" Newspaper_"+tdate+".pdf")
+
+        r = requests.get(url_hindu) 
+        with open("TH_Newspaper_"+tdate+".pdf",'wb') as f: 
+
+
+            f.write(r.content)
+        print("sending email..."+"IE_Newspaper_"+tdate+".pdf"+","+"TH_Newspaper_"+tdate+".pdf")
         
         ################################################################################
 
-        send_mail("thecolossus018@gmail.com",["kumarisuruchi707@gmail.com","rsumit123@gmail.com","rpuja132@gmail.com","gogetmayank23@gmail.com"],date+" Indian Express","Greetings from Sumit's Bot , Find today's Indian Express paper in attachment",files = ["Newspaper_"+tdate+".pdf"])
-        send_mail("thecolossus018@gmail.com",["praachi.nk@gmail.com"],date+" Indian Express","Greetings from Sumit's Bot , Find today's Indian Express paper in attachment",files = ["Newspaper_"+tdate+".pdf"])
-        # send_mail("thecolossus018@gmail.com",["rsumit123@gmail.com"],date+" Indian Express","Greetings from Sumit's Bot , Find today's Indian Express paper in attachment",files = ["Newspaper_"+tdate+".pdf"])
+        send_mail("thecolossus018@gmail.com",["kumarisuruchi707@gmail.com","rsumit123@gmail.com","rpuja132@gmail.com","gogetmayank23@gmail.com","praachi.nk@gmail.com"],date+" Indian Express and The Hindu","Greetings from Sumit's Bot , Find today's Indian Express and The Hindu paper in attachment",files = ["IE_Newspaper_"+tdate+".pdf","TH_Newspaper_"+tdate+".pdf"])
+        # send_mail("thecolossus018@gmail.com",["praachi.nk@gmail.com"],date+" Indian Express and The Hindu","Greetings from Sumit's Bot , Find today's Indian Express and The Hindu paper in attachment",files = ["IE_Newspaper_"+tdate+".pdf","TH_Newspaper_"+tdate+".pdf"])
+        # send_mail("thecolossus018@gmail.com",["rsumit123@gmail.com"],date+" Indian Express and The Hindu","Greetings from Sumit's Bot , Find today's Indian Express paper in attachment",files = ["IE_Newspaper_"+tdate+".pdf","TH_Newspaper_"+tdate+".pdf"])
 
         return 1
     else:
@@ -211,8 +209,10 @@ downloaded = 0
  
 while downloaded == 0:
     downloaded = download_pdf()
-    print("Paper not available yet, Trying again after 120 secs")
-    time.sleep(150)
-    
+    if downloaded==0:
+        print("Paper not available yet, Trying again after 1000 secs")
+        time.sleep(1000)
+    else:
+        break
 
 
